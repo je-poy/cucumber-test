b5f45e440100f84ec2c80facfcb32ae04b6081fc
Jeff <canalejeff@gmail.com>
2019-02-19 11:15:57 +0800

    Added scenario for checking skmti object method toReqParams

--- features/sk_util.feature
+++ features/sk_util.feature
@@ -9,3 +9,8 @@ Feature: SKMTI Suitescript Utilities
       Given you have a JSON object
       When the object has properties with empty value
       Then properties should be removed
+
+    Scenario: Add query params using JSON Object into URL string
+      Given your URL is 'www.cucumbersample.com'
+      When parameter is provided
+      Then the params should be appended on the URL
--- features/step_definitions/sk_object_steps.js
+++ features/step_definitions/sk_object_steps.js
@@ -4,6 +4,7 @@ const SK = require('../../src/SKUtils.js');
 
 Given('you provide a JSON Object', function() {
   this.hasEmptySource = { a: 1, b: null, c:2, d: "", f: undefined };
+  this.paramSource = { name: "Jeff", age: 23 };
 });
 
 Given('you have a JSON object', function() {
@@ -17,4 +18,18 @@ When('the object has properties with empty value', function() {
 Then('properties should be removed', function() {
   var expected = { a:1, c:2 };
   assert.ok(SK.object.compare(this.hasEmptySource, expected));
+});
+
+Given('your URL is {string}', function(urlStr) {
+  this.urlStr = urlStr;
+});
+
+When('parameter is provided', function(){
+  assert.ok(this.paramSource, "You should have a JSON Object for url parameters");
+});
+
+Then('the params should be appended on the URL', function() {
+  var urlWithParams = SK.object.toReqParams(this.urlStr, this.paramSource);
+  var expected = this.urlStr+"?name=Jeff&age=23";
+  assert.equal(urlWithParams, expected);
 });
